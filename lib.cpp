b9bed79ef0ad5967642a2afbb3aa4e4465288a64
S5577727 <115453720+S5577727@users.noreply.github.com>
2022-12-07 19:56:57 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,17 +1,20 @@
 #include "lib.h"
 
 bool funzione (char carattere) {
-    if (((char)97<=carattere and carattere<=(char)122) or ((char)65<=carattere and carattere<=(char)90)){
-        return true;    //se è una lettera
+    if ((((char)97<=carattere) and (carattere<=(char)122)) or (((char)65<=carattere) and (carattere<=(char)90))){
+        return true;
     }
-    else return false;   // se non è una lettera
+    else {
+        return false;
+    }
 }
 
 char conversione (char carattere){
-    if ('a'<=carattere and carattere<='z') {
+    if (('a'<=carattere) and (carattere<='z')) {
         carattere-=(char)32;
     }
-    else carattere+=(char)32;
-
+    else {
+        carattere+=(char)32;
+    }
     return carattere;
 }
