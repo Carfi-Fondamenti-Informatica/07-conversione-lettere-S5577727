3c419ab76793d76cfe9aa66e9794f5da4f70b422
S5577727 <115453720+S5577727@users.noreply.github.com>
2022-12-07 19:48:12 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+#include "lib.h"
 
+bool funzione (char carattere) {
+    if (((char)97<=carattere and carattere<=(char)122) or ((char)65<=carattere and carattere<=(char)90)){
+        return true;    //se è una lettera
+    }
+    else return false;   // se non è una lettera
+}
+
+char conversione (char carattere){
+    if ('a'<=carattere and carattere<='z') {
+        carattere-=(char)32;
+    }
+    else carattere+=(char)32;
+
+    return carattere;
+}
