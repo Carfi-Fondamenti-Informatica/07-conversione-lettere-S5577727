a548cd1003d825968383057c57e09fc59ded5075
S5577727 <115453720+S5577727@users.noreply.github.com>
2022-12-07 19:48:15 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,5 @@
-bool funzione (char carattere)
+#ifndef ESERCITAZIONE_7_NICK_LIB_H
+#define ESERCITAZIONE_7_NICK_LIB_H
+bool funzione (char carattere);
+char conversione (char carattere);
+#endif //ESERCITAZIONE_7_NICK_LIB_H
